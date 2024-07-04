f304d6b9ed592578e5039d74f4bb2d8702382d95
Academyesp <166895503+Academyesp@users.noreply.github.com>
2024-07-04 18:06:15 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDO4TGz4QUpRCkrgNwRSGNV8_ADgQCpKQU",
+    authDomain: "academy-esp.firebaseapp.com",
+    projectId: "academy-esp",
+    storageBucket: "academy-esp.appspot.com",
+    messagingSenderId: "313752768894",
+    appId: "1:313752768894:web:604e93bcb50bedcd62232f",
+    measurementId: "G-0BPKEBYJRK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
